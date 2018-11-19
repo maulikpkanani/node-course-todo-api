d3f965702ad2008bb0be59d458d98c71e0af5bf7
Maulik Kanani <maulikkanani1894@gmail.com>
2018-11-19 13:42:15 -0500

    Add tests for POST/user/login

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -2,10 +2,10 @@ const expect = require('expect');
 const request = require('supertest');
 const { ObjectID } = require('mongodb');
 
-const { app } = require('./../server');
-const { Todo } = require('./../models/todo');
-const { User } = require('./../models/user');
-const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
+const { app } = require('../../server');
+const { Todo } = require('../../models/todo');
+const { User } = require('../../models/user');
+const { todos, populateTodos, users, populateUsers } = require('../seed/seed');
 
 beforeEach(populateUsers);
 beforeEach(populateTodos);
